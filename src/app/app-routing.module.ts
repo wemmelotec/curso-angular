4874d0f7581e769b4daf3f92cb192b37298e31ce
wellington magno <tonmelo@hotmail.com>
2022-12-12 13:40:40 -0300

    end of class 18

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { TwoWayBidingComponent } from './components/two-way-biding/two-way-biding.component';
+
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'parent', component: ParentDataComponent },
+  { path: 'towaybiding', component: TwoWayBidingComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
